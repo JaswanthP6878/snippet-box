1c7cb2a630edfcc2312b6dd4097e9ffaf306f60f
JaswanthP6878 <klucse2000039034@gmail.com>
2023-04-21 21:04:05 +0530

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module snippetbox.jaswanthp.com
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+  "log"
+  "net/http"
+)
+
+// home is a handler function
+func home(w http.ResponseWriter, r *http.Request) {
+  w.Write([]byte("Hello from snippetbox"))
+}
+
+func main() {
+  mux := http.NewServeMux()
+  mux.HandleFunc("/", home)
+
+
+  log.Println("Starting server on 4000:")
+  err := http.ListenAndServe(":4000", mux)
+
+  log.Fatal(err)
+
+}
