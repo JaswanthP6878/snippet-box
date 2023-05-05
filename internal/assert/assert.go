cbf29ada31cad0ff155ac144813c36cd39049eca
JaswanthP6878 <klucse2000039034@gmail.com>
2023-05-05 20:40:19 +0530

    added handler and middlware tests

--- internal/assert/assert.go
+++ internal/assert/assert.go
@@ -9,7 +9,7 @@ func Equal[T comparable](t *testing.T, actual, expected T) {
 	// to annotate the go test runner that this function
 	// is a helper function
 	t.Helper()
-	if actual != actual {
+	if actual != expected {
 		t.Errorf("got %v; want %v", actual, actual)
 	}
 }
